96fc47e6243640e836148f4136bcb627e7d28d62
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-06-07 22:39:56 +0530

    fistSboot

---
+++ spring-app2/src/main/java/com/example/demo/SpringApp2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApp2Application.class, args);
+
+		System.out.println("Hii Spring Rahul");
+	}
+
+}
